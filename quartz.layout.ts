86c546a87e74cd3ba31968713d037149f3722130
merlinuwe <uwe.wennmann@gmail.com>
2024-05-04 11:54:36 +0200

    Quartz sync: May 4, 2024, 11:54 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,6 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.RecentNotes({ title: "Recent writing" })
     Component.Backlinks(),
   ],
 }
