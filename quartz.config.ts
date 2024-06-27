56c04e58c96fba9f9580f36584664944942db30f
merlinuwe <uwe.wennmann@gmail.com>
2024-06-27 19:05:47 +0200

    Quartz sync: Jun 27, 2024, 7:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Wm",
+    pageTitle: " Wm",
     enableSPA: true,
     enablePopovers: true,
     analytics: "none",
